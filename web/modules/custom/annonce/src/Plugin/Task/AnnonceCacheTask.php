b8ea7dfc5eb8ba57277ee64f3262fe1660fe39ea
Nelson <p.nelson786@gmail.com>
2019-11-15 17:35:17 +0100

    module annonce start

---
+++ web/modules/custom/annonce/src/Plugin/Task/AnnonceCacheTask.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Drupal\annonce\Plugin\Task;
+
+use Drupal\Core\Menu\LocalTaskDefault;
+
+class AnnonceCacheTask extends LocalTaskDefault {
+
+  public function getCacheContexts() {
+    return ['user'];
+  }
+
+}
